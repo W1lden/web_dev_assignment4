741aa2348ec3d8386715d1b2e5cc8faba3b7493c
Асанали Битанов <asan@MacBook-Air-Asanali.local>
2024-11-20 15:28:45 +0500

    second commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,11 @@
-from django.urls import path
-from .views import (
-    PostListCreateView,
-    PostRetrieveUpdateDestroyView,
-    CommentListCreateView,
-    CommentRetrieveUpdateDestroyView,
-)
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PostViewSet, CommentViewSet
+
+router = DefaultRouter()
+router.register(r'posts', PostViewSet, basename='post')
+router.register(r'comments', CommentViewSet, basename='comment')
 
 urlpatterns = [
-    path('posts/', PostListCreateView.as_view(), name='post-list-create'),
-    path('posts/<int:pk>/', PostRetrieveUpdateDestroyView.as_view(), name='post-detail'),
-    path('comments/', CommentListCreateView.as_view(), name='comment-list-create'),
-    path('comments/<int:pk>/', CommentRetrieveUpdateDestroyView.as_view(), name='comment-detail'),
+    path('api/', include(router.urls)),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,11 @@
-from rest_framework import generics
+from rest_framework import viewsets
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 
-# View для списка постов и создания нового поста
-class PostListCreateView(generics.ListCreateAPIView):
+class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-# View для получения, обновления или удаления конкретного поста
-class PostRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-# View для комментариев
-class CommentListCreateView(generics.ListCreateAPIView):
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
-
-    def perform_create(self, serializer):
-        # Убедимся, что post_id передается и создается связанный комментарий
-        serializer.save()
-
-# View для получения, обновления или удаления конкретного комментария
-class CommentRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
